c2cabdc3493102fd49a3dbc3b9790d2f8a600ce8
0778430 <0778430@ACIS-TUTOR01-P.pittstate.edu>
2020-03-04 10:28:35 -0600

    paint done?

--- Lab3/src/main/java/Paint_Job_Estimator.java
+++ Lab3/src/main/java/Paint_Job_Estimator.java
@@ -22,8 +22,7 @@ Then it should display the data on the screen
      */
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        final int ONE_GALLON_OF_PAINT_PER_HOUR = 8;
-        final double HOUR_OF_LABER = 18.00;
+        
         double numberOfSquareFeet = 0.00;
 
         System.out.println("Please enter the number of rooms");
@@ -38,41 +37,55 @@ Then it should display the data on the screen
             numberOfRooms = numberOfRooms - 1;
         }
         System.out.println("Please enter the price of paint per gallon");
-        double priceOfPainPerGallon = keyboard.nextDouble();
-        //output  number of gallons of paint required
+        double priceOfPaintPerGallon = keyboard.nextDouble();
         double outputOfPaint = numberOfGallonsOfPaint(numberOfSquareFeet);
-        System.out.println("You need "+outputOfPaint+ "gallons of paint ");
+        double outputOfPaintCost = theCostOfPaint(outputOfPaint, priceOfPaintPerGallon);
+        double outputOfLaborNeed =numberOfHourOfLaber(numberOfSquareFeet);
+        double outputOfHourCharges=numberOfHoursCharges(outputOfLaborNeed);
+        double outputTotal=TotalCost(outputOfPaintCost,outputOfHourCharges);
+        //The number of gallons of paint required
+        System.out.println("You need " + outputOfPaint + "gallons of paint ");
+        //The cost of the paint
+        System.out.println("the cost of paint is $" + outputOfPaintCost);
         // output The hours of labor required
-        //output The cost of the paint
+        System.out.println("The number of hours need to do this procject is "+ outputOfLaborNeed);
         //output The labor charges
+        System.out.println("The total number of hours charges for wage is " +outputOfHourCharges );
         //output The total cost of the paint job
+        System.out.println("the total cost is $"+outputTotal);
     }
-    public static double numberOfGallonsOfPaint(double numberOfSquareFeet){
-         int gallonsOfPaint=0;
-       do {
-         gallonsOfPaint=gallonsOfPaint+1;
-         numberOfSquareFeet=numberOfSquareFeet-115;
+
+    public static double numberOfGallonsOfPaint(double numberOfSquareFeet) {
+        int gallonsOfPaint = 0;
+        do {
+            gallonsOfPaint = gallonsOfPaint + 1;
+            numberOfSquareFeet = numberOfSquareFeet - 115;
         } while (numberOfSquareFeet > 0);
-         
-         
-        
-                return gallonsOfPaint;
+
+        return gallonsOfPaint;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    public static double theCostOfPaint(double outputOfPaint, double priceOfPaintPerGallon) {
+        double costOfPaint = outputOfPaint * priceOfPaintPerGallon;
+        return costOfPaint;
+    }
+
+    public static double numberOfHourOfLaber(double numberOfSquareFeet){
+        double numberOfHours=0;
+        do {
+            numberOfHours = numberOfHours + 8;
+            numberOfSquareFeet = numberOfSquareFeet - 115;
+        } while (numberOfSquareFeet > 0);
+        return numberOfHours;
+    }
+    public static double numberOfHoursCharges(double outputOfLaborNeed){
+        double hoursCharges =outputOfLaborNeed*18;
+        return hoursCharges;
+    }
+    public static double TotalCost(double outputOfPaintCost, double outputOfHourCharges){
+        double total=outputOfPaintCost+outputOfHourCharges;
+        return total;
+    }
     
     
     
@@ -84,10 +97,3 @@ Then it should display the data on the screen
     
     
 }
-
-
-
-
-
-
-
